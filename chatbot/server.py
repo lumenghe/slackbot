4b07b6e88045534a88d5c05e79c487a6f05e2920
lumenghe <lumenghe@gmail.com>
2017-08-05 18:59:27 +0100

    adding switch

--- chatbot/server.py
+++ chatbot/server.py
@@ -27,3 +27,21 @@ def getbotname():
 def home():
     return "{} chatbot is running here :-)".format(name)
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Chatbot arguments')
+    parser.add_argument('-n','--name', help='name of the chatbot', type=str, required=True)
+    parser.add_argument('--host', help='host for chatbot', type=str, default="0.0.0.0")
+    parser.add_argument('-p','--port', help='port for chatbot', type=int, required=True)
+    parser.add_argument('-b', '--bot', help='bot (English,Zen, or Math)', type=str, required=True)
+    parser.add_argument('-w', '--workdir', help='work directory', type=str, default=os.getcwd())
+    args = parser.parse_args()
+    name = args.name
+    host = args.host
+    port = args.port
+    botname = args.bot
+    workdir = args.workdir
+    print("Initializing chabot... ", end="")
+    chatbot = load_chatbot(botname, name, port, workdir)
+    print("done.")
+    app.run(debug=True, host=host,port=port)
