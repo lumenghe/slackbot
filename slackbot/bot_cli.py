989cd84a562fb539ddd724edf2aa54e064d9242e
lumenghe <lumenghe@gmail.com>
2017-08-14 09:39:50 +0100

    new function 'ask_from_cli'

--- slackbot/bot_cli.py
+++ slackbot/bot_cli.py
@@ -11,3 +11,14 @@ chatservice_host = None
 active_bots = dict()
 current_bot = None
 
+
+@app.route('/askfromcli', methods=['GET'])
+def ask_from_cli():
+    """
+    Receive text from CLI interface
+    """
+    global current_bot
+    global active_bots
+    text = request.args.get('q')
+    reply, current_bot, active_bots = parse_message(text, current_bot, active_bots, chatservice_host)
+    return jsonify({'response': reply})
